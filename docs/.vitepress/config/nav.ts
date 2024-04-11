5c51d7f3bc39542ae5027bc5582d0b07608781e8
张宇豪 <2756864799@qq.com>
2024-04-11 23:39:12 +0800

    更新项目文档

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -4,10 +4,10 @@ export const nav: DefaultTheme.Config['nav'] = [
   {
     text: '快速开始',
     items: [
-      { text: '智能合约设计', link: '/categories/start/快速开始/03/29/GivTech慈善公益智能合约设计', activeMatch: '/categories/start/' },
-      { text: '后端开发设计', link: '/categories/start/快速开始/03/29/GivTech慈善公益后端项目设计', activeMatch: '/categories/start/' },
-      { text: '前端开发设计', link: '/categories/start/快速开始/03/29/GivTech慈善公益前端项目设计', activeMatch: '/categories/start/' },
-      { text: '运维部署设计', link: '/categories/start/快速开始/03/29/GivTech慈善公益运维部署设计', activeMatch: '/categories/start/' }
+      { text: '智能合约设计', link: '/categories/start/系统模块/03/29/GivTech慈善公益智能合约设计', activeMatch: '/categories/start/' },
+      { text: '后端开发设计', link: '/categories/start/系统模块/03/29/GivTech慈善公益后端项目设计', activeMatch: '/categories/start/' },
+      { text: '前端开发设计', link: '/categories/start/系统模块/03/29/GivTech慈善公益前端项目设计', activeMatch: '/categories/start/' },
+      { text: '运维部署设计', link: '/categories/start/系统模块/03/29/GivTech慈善公益运维部署设计', activeMatch: '/categories/start/' }
     ],
     activeMatch: '/categories/'
   },
